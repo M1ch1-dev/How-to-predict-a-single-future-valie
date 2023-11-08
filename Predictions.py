e5108da9aaa755ceb808fa38dd9bc512562df249
M1ch1-dev <122740999+M1ch1-dev@users.noreply.github.com>
2023-11-08 12:49:49 -0400

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -33,3 +33,31 @@ for i in range(0, 10):
     ax.set_ylabel('Number of liters')
     ax.set_axisbelow(True)
     plt.show()
+
+#So.... what grade should we pick?
+
+year = 2000
+grade = np.arange(0, 20 + 1, 1) #from 0 to 100 but 1 by 1
+aprox = np.array([])
+y_pred_vec = np.array([])
+
+for i in grade:
+    coef = np.polyfit(x, y, i)
+    p = np.polyval(coef, year)
+    aprox = np.append(aprox, p)
+
+    #To do the MSE(Mean Squared Error)
+    y_pred_vec = np.array([])
+    for j in x:
+        y_pred = np.polyval(coef, j)
+        y_pred_vec = np.append(y_pred_vec, y_pred)
+    #print(f'los y: {y}')
+    #print(f'the y_prox: {y_pred_vec}')
+    MSE = (sum( (y - y_pred_vec)**2))/len(y)
+    print(f'at grade {i} the MSE is {MSE}')
+plt.figure(figsize= [20, 10])
+plt.title('Polynomial grade vs prediction')
+plt.plot(grade, aprox, '--', linewidth= 3, color= 'red')
+plt.grid('on')
+plt.show()
+
